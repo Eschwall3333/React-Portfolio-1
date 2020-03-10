57b0f25d7be1a461b54e4c845c2a1aed7a388413
Emory Schwall <emory3@comcast.net>
2020-03-10 11:55:56 -0400

    first push

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,53 +1,42 @@
 let resumeData = {
     "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
+    "name": "Emory A. Schwall III",
+    "role": "Full stack web developer and software engineer",
     "linkedinId":"Your LinkedIn Id",
     "skypeid": "Your skypeid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, work on cars, produce music or simply play games in my free time.",
     "socialLinks":[
         {
           "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
+          "url":"https://www.linkedin.com/in/emory-schwall-7a146a158/",
           "className":"fa fa-linkedin"
         },
         {
           "name":"github",
-          "url":"http://github.com/rbhatia46",
+          "url":"https://github.com/Eschwall3333",
           "className":"fa fa-github"
-        },
-        {
-          "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
-          "className":"fa fa-twitter"
         }
       ],
-    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "aboutme":"Junior full stack web developer dedicated to efficiency, clean code, innovation and contributing to company success. Creative, reliable and always improving, with strong time management skills and prioritization abilities. I'm a self taught software engineer with a growing passion for Java and c++.  I love frontend development, building custom software applications and overall a good challenge.",
     "address":"India",
     "website":"https://rbhatia46.github.io",
     "education":[
       {
-        "UniversityName":"The LNM Insitute of Information Technology",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2020",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
+        "UniversityName":"Georgia Institute of Technology",
+        "specialization":"Full Stack Web Development (MERN and MEAN)",
+        "MonthOfPassing":"Nov",
+        "YearOfPassing":"2019",
+        "Achievements":"Certificate of Completion - Full Stack Web Development"
       }
+    
     ],
     "work":[
       {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
+        "CompanyName":"Proveli",
+        "specialization":"Inventory Control Specialist",
+        "MonthOfLeaving":"Feb",
+        "YearOfLeaving":"2020",
+        "Achievements":"Apply knowledge of Six Sigma strategy to creatively troubleshoot stocking issues to provide resolution or temporary workarounds. "
       },
       {
         "CompanyName":"Some Company",
